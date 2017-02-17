3338da5a9a2dcba5731272b67fc78a7d1740fa2f
LGrok <luolin.work@gmail.com>
2017-02-17 08:39:31 +0800

    fix aiohttp bug. Use session out side coroutine.

--- dirs.py
+++ dirs.py
@@ -21,8 +21,14 @@ class Dirscaner:
         self.max_tries = max_tries
         self.loop = loop or asyncio.get_event_loop()
         self.q = asyncio.Queue(loop=self.loop)
-        self.session = aiohttp.ClientSession(loop=self.loop)
+        self._session = None
         self.error404_flag = False
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = aiohttp.ClientSession(loop=self.loop)
+        return self._session
 
     def close(self):
         self.session.close()
@@ -43,18 +49,35 @@ class Dirscaner:
         random_nums = [random.randint(0,len(content)) for i in range(num)]
         return [content[i:i+lenth] for i in random_nums]
 
-    def fetch_error(self, host):
+    def fetch_error(self, host, num=4):
+        """
+        获取错误页面关键词
+        :param host:
+        :param num: 提取关键词数量
+        :return: error keys
+        """
         randoms = [self.fetch_random(host) for i in range(4)]
         status = [statu for statu,content in randoms]
         contents = [content for statu,content in randoms]
-        self.catch_web_strs(contents[0], 20)
+        self.catch_web_strs(contents[0], 20, num)
         if list(set(status)) == 404:
             self.error404_flag = True
         else:
-            keys = self.catch_web_strs(contents[0], 20)                  #确定关键字是否在其他页面出现
+            import time
+            keys = self.catch_web_strs(contents[0], 20, num)                  #确定关键字是否在其他页面出现
+            while True:
+                num = len(keys)
+                for k in keys:
+                    for content in contents[1:]:
+                        if k not in content:
+                            keys.remove(k)
+                if len(keys) == num:
+                    break
 
+        return keys
 
 if __name__=="__main__":
     d = Dirscaner("http://www.baidu.com")
-    d.fetch_error(host="http://www.baidu.com")
-    d.close()
+    keys = d.fetch_error(host="http://www.baidu.com")
+    print(keys)
+
